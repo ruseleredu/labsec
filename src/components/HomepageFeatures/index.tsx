20e7bc28b7c608c209866679b9dd0eb73df654df
Adriano Ruseler <adrianoruseler@gmail.com>
2025-10-16 11:10:43 -0300

    Add frontpage logo

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -2,41 +2,52 @@ import type {ReactNode} from 'react';
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import Link from "@docusaurus/Link";
 
 type FeatureItem = {
-  title: string;
+  title: React.ReactNode; // Changed to ReactNode to allow links
   Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: ReactNode;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: (
+      <Link to="https://www.utfpr.edu.br">
+        Universidade Tecnológica Federal do Paraná (UTFPR)
+      </Link>
+    ),
+    Svg: require("@site/static/img/UTFPR_logo.svg").default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        A UTFPR é uma das universidades mais renomadas do Brasil. O seu padrão de ensino é inspirado no modelo de universidade politécnica europeia. É especializada em ciências naturais e engenharia.
+      </>
+    ),
+  }, 
+  {
+    title: (
+      <Link to="https://utfpr.curitiba.br/daelt/">
+        Departamento Acadêmico de Eletrotécnica (DAELT)
+      </Link>
+    ),
+    Svg: require("@site/static/img/LogoDAELT.svg").default,
+    description: (
+      <>
+        O Departamento Acadêmico de Eletrotécnica do Câmpus Curitiba da UTFPR é responsável pela organização das atividades de ensino, pesquisa e extensão na área de Engenharia Elétrica e de Controle.
       </>
     ),
   },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
+    {
+    title: (
+      <Link to="http://www.daeln.ct.utfpr.edu.br/">
+        Departamento Acadêmico de Eletrônica (DAELN)
+      </Link>
     ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Svg: require("@site/static/img/UTFPR_logo.svg").default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        O
+Departamento Acadêmico de Eletrônica do Câmpus Curitiba da UTFPR é a unidade responsável por coordenar e ministrar os cursos e disciplinas da área de Eletrônica.
       </>
     ),
   },
